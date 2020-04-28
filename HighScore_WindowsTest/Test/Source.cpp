70dee166bb9afa33f3130b2aaa49b7f6cda55b80
theixgamer <theofritaku@gmail.com>
2020-04-28 13:53:23 -0300

    glut incorporado al proyecto

--- HighScore_WindowsTest/Test/Source.cpp
+++ HighScore_WindowsTest/Test/Source.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include "../glui_2.37_x86_msvc2015/include/GL/freeglut.h"
+#include "../glui_2.37_x86_msvc2015/include/GL/glut.h"
 using namespace std;
 
-void main()
+void Display_cb() {
+	glutSwapBuffers();
+}
+int main(int argc, char** argv) 
 {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+	glutInitWindowSize(480, 360);
+	glutInitWindowPosition(50, 50);
+	glutCreateWindow("Test");
 
+	glutDisplayFunc(Display_cb);
+	glutMainLoop();
+	return 0;
 }
